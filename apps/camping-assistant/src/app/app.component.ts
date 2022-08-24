3ded6e3535cbaf80b050d4a6344be0ec2c44ad78
Preston Lamb <preston.j.lamb@gmail.com>
2022-08-23 19:00:54 -0600

    initial commit

---
+++ apps/camping-assistant/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'camping-assistant-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'camping-assistant';
+}
