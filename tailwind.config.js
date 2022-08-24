5b30467e98e8c66fc61e0a910e8deb91e4119161
Preston Lamb <preston.j.lamb@gmail.com>
2022-08-23 19:03:14 -0600

    chore: install primeng and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
