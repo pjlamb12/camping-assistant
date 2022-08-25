f62c71160c6669ab32f102b906d969a0509b7af8
Preston Lamb <preston.j.lamb@gmail.com>
2022-08-24 23:27:10 -0600

    feat: list display data
    Show more data on the list display

--- libs/camping-assistant/ui/trips/src/lib/trip-list-display/trip-list-display.component.ts
+++ libs/camping-assistant/ui/trips/src/lib/trip-list-display/trip-list-display.component.ts
@@ -2,9 +2,18 @@ import {
 	ChangeDetectionStrategy,
 	Component,
 	Input,
+	OnChanges,
 	OnInit,
+	SimpleChanges,
 } from '@angular/core';
-import { Trip } from '@camping-assistant/camping-assistant/data-access/trips';
+import {
+	Trip,
+	TripStep,
+} from '@camping-assistant/camping-assistant/data-access/trips';
+
+interface ExtendedTrip extends Trip {
+	isComplete: boolean;
+}
 
 @Component({
 	selector: 'ca-trip-list-display',
@@ -12,9 +21,21 @@ import { Trip } from '@camping-assistant/camping-assistant/data-access/trips';
 	styleUrls: ['./trip-list-display.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TripListDisplayComponent implements OnInit {
+export class TripListDisplayComponent implements OnChanges {
 	@Input() trips!: Trip[];
+	public extendedTrips: ExtendedTrip[] = [];
 	constructor() {}
 
-	ngOnInit(): void {}
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['trips']) {
+			this.extendedTrips = this.trips.map((trip: Trip) => {
+				return {
+					...trip,
+					isComplete: trip.steps.every(
+						(step: TripStep) => step.completed === true
+					),
+				};
+			});
+		}
+	}
 }
