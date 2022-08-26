c5bc8d29a90209c4e0576543cfd3f11aceae5954
Preston Lamb <preston.j.lamb@gmail.com>
2022-08-25 23:43:18 -0600

    fix: theme color and page titles

--- libs/camping-assistant/feature/trips/src/lib/camping-assistant-feature-trips.module.ts
+++ libs/camping-assistant/feature/trips/src/lib/camping-assistant-feature-trips.module.ts
@@ -12,11 +12,27 @@ import { TripDetailComponent } from './trip-detail/trip-detail.component';
 		CommonModule,
 		CampingAssistantUiTripsModule,
 		RouterModule.forChild([
-			{ path: 'add/:id', component: TripFormComponent },
-			{ path: 'copy/:id', component: TripFormComponent },
-			{ path: 'edit/:id', component: TripFormComponent },
-			{ path: 'view/:id', component: TripDetailComponent },
-			{ path: '', component: TripListComponent },
+			{
+				path: 'add/:id',
+				component: TripFormComponent,
+				title: 'Add Trip',
+			},
+			{
+				path: 'copy/:id',
+				component: TripFormComponent,
+				title: 'Duplicate Trip',
+			},
+			{
+				path: 'edit/:id',
+				component: TripFormComponent,
+				title: 'Edit Trip',
+			},
+			{
+				path: 'view/:id',
+				component: TripDetailComponent,
+				title: 'View Trip',
+			},
+			{ path: '', component: TripListComponent, title: 'View Trips' },
 		]),
 		ReactiveFormsModule,
 	],
